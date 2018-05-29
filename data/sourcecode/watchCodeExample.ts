aefdc865b5d630774837411b3fbd883e128d4f15
Leo Haufs <haufsleo@users.noreply.github.com>
2018-05-29 16:48:35 +0200

    Initial ShareLaTeX Import

---
+++ data/sourcecode/watchCodeExample.ts
@@ -0,0 +1,42 @@
+/* 
+Die in XCode festgelegte appGroupId "eventAppGroup" und die Variable
+successfullWatchInit sind global verfuegbar oder ueber einen 
+entsprechenden Mechanismus (z.B. local storage) global erreichbar. 
+*/
+var appGroupId = "eventAppGroup";
+var successfullWatchInit = false;
+
+/* Bei Appstart wird aufgerufen: */
+var initPlattformAccess = () => {
+    if (this.platform.is('ios')) {
+        applewatch.init(() => {
+            // success
+            successfullWatchInit = true;
+        }, () => {
+            // error
+            successfullWatchInit = false;
+        }, appGroupId);
+    }
+}
+
+/* Definition der zu sendenden Daten als Array von JSON-Objekten */
+var data = [{
+    type: 'talk',
+    title: 'Keynote',
+    startDate: (new Date(2017, 1, 14, 08, 0, 0)).toISOString(),
+    endDate: (new Date(2017, 1, 14, 10, 0, 0)).toISOString(),
+    talkerName: 'Dr. Karl Mustermann',
+    location: 'Großer Hauptsaal',
+    isFavorite: true
+}, /* ... */
+];
+
+/* Senden an die Apple Watch als Teil der User Defaults */
+if (successfullWatchInit) {
+    applewatch.sendUserDefaults(() => {
+        console.log("successfully send data");  // success
+    }, () => {
+        console.log("error sending data");      // error
+    },
+        { "database": JSON.stringify(data) }, appGroupId);
+}
